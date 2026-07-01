84b43cef1ff7f1deea66dce3274859c36a80e38b
PatrickCreatesStuff <128487722+patricklarid@users.noreply.github.com>
2026-07-02 00:50:27 +0300

    Restructuring directory

--- China_Expenses_Analysis/SQL/Spend By Category.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    Category,
-    ROUND(SUM(Amount), 2) AS Total_Spend,
-    COUNT(*) AS Transactions,
-    ROUND(AVG(Amount), 2) AS Avg_Transaction,
-    ROUND(SUM(Amount) * 100.0 / (SELECT SUM(Amount) FROM expenses), 1) AS Budget_Percentage
-FROM expenses
-GROUP BY Category
-ORDER BY Total_Spend DESC;
